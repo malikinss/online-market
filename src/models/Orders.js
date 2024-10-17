22fa42aca3f52565fd150b9db077d34ede8c8e92
Sam Malikin <workmalikin@gmail.com>
2024-10-17 18:50:11 +0300

    Created an order model using Sequelize
    - Defined an `Order` model with attributes: `id`, `totalPrice`, and `status`.
    - The `totalPrice` field is defined as a decimal number with 10 precision and 2 decimal places, with mandatory validation.
    - The `status` field has a default value of "Created" and is validated based on a limited list of values.
    - Uses timestamps to track when a record was created and updated.

---
+++ src/models/Orders.js
@@ -0,0 +1,41 @@
+const sequelize = require("../config/dbConnect");
+const { DataTypes } = require("sequelize");
+const {
+  validationRules,
+  createValidation,
+  createValidationIsIn,
+} = require("../utils/validationHandling");
+
+const Order = sequelize.define(
+  "order",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    totalPrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: createValidation(validationRules.order.price, "total price"),
+    },
+
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "Created",
+      validate: createValidationIsIn(
+        validationRules.order.status,
+        "order status"
+      ),
+    },
+  },
+  {
+    tableName: 'orders',
+    timestamps: true,
+  }
+);
+
+module.exports = Order;
