53bc95893cd3cad2916138118126a746114f7fda
Sam Malikin <workmalikin@gmail.com>
2024-10-17 18:47:34 +0300

    Created a UserAddress model using Sequelize
    - Defined a `UserAddress` model with attributes: `id`, `country`, `city`, `street`, `building`, `apartment`, `postal`.
    - Added validation for the `country`, `city`, `street`, `building`, and `postal` fields using custom validation rules.
    - Made the `apartment` field optional.
    - Use timestamps to track when a record was created and updated.

---
+++ src/models/UserAddresses.js
@@ -0,0 +1,66 @@
+const sequelize = require("../config/dbConnect");
+const { DataTypes } = require("sequelize");
+const {
+  validationRules,
+  createValidation,
+} = require("../utils/validationHandling");
+
+const UserAddress = sequelize.define(
+  "address",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    country: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: createValidation(validationRules.address.country, "country"),
+    },
+
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: createValidation(validationRules.address.city, "city"),
+    },
+
+    street: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: createValidation(validationRules.address.street, "street"),
+    },
+
+    building: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: createValidation(
+        validationRules.address.building,
+        "building number"
+      ),
+    },
+
+    apartment: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      validate: createValidation(
+        validationRules.address.apartment,
+        "apartment number"
+      ),
+    },
+
+    postal: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: createValidation(validationRules.address.postal, "postal code"),
+    },
+  },
+  {
+    tableName: 'addresses',
+    timestamps: true,
+  }
+);
+
+module.exports = UserAddress;
