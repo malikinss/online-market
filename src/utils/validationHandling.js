56223996e01cd7e69db4818fcc5cf3e4ecccc6c2
Sam Malikin <workmalikin@gmail.com>
2024-10-17 18:53:11 +0300

    Created a utility for validation using regular expressions
    - Defined popularRegExps for validating geographical names, usernames, positive numbers and monetary values.
    - Added validation rules for various entities:
    - `user`: validate first name, last name, phone number and password.
    - `address`: validate country, city, street, building number, apartment number and postal code.
    - `order`: validate price and order status.
    - Implemented `createValidation` and `createValidationIsIn` functions for creating validation objects with custom error messages.

---
+++ src/utils/validationHandling.js
@@ -0,0 +1,72 @@
+const { messages } = require("../views/messageHandling");
+
+popularRegExps = {
+  geographicName: /^[A-Za-z\s\-’'é]{2,100}$/, // Geographic names (countries and cities)
+  personalName: /^[a-zA-Z]{2,20}$/, // User names
+  positiveNumber: /^\d+$/, // Positive integer
+  moneyValue: /^\d{1,8}(\.\d{1,2})?$/, // Money value with two decimal places
+};
+
+const validationRules = {
+  user: {
+    firstName: popularRegExps.personalName,
+    lastName: popularRegExps.personalName,
+    phone: /^0\d{9}$/, // Phone number format: 0XXXXXXXXX
+    password: /^[a-zA-Z0-9!@#$%^&*]{8,20}$/, // Password with allowed characters
+    roles: ["user", "admin"], // User roles
+  },
+
+  address: {
+    country: popularRegExps.geographicName,
+    city: popularRegExps.geographicName,
+    street: popularRegExps.geographicName,
+    building: popularRegExps.positiveNumber,
+    appartment: popularRegExps.positiveNumber,
+    postal: /^\d{7}$/, // Postal code format: 7 digits long
+  },
+
+  order: {
+    price: popularRegExps.moneyValue,
+    status: [
+      "Created",
+      "Unpaid",
+      "Paid",
+      "Processing",
+      "Shipped",
+      "Delivered",
+      "Canceled",
+      "Returned",
+    ],
+  },
+};
+
+
+/**
+ * Creates a validation object for the specified rule and field name.
+ * 
+ * @param {RegExp} rule - The regular expression for validating the field.
+ * @param {string} fieldName - The name of the field for which validation is created.
+ * @returns {Object} The validation object with the is rule.
+ */
+const createValidation = (rule, fieldName) => ({
+  is: {
+    args: rule,
+    msg: messages.error.requirements(fieldName),
+  },
+});
+
+/**
+ * Creates a validation object for checking if the value is in the specified list.
+ * 
+ * @param {Array} rule - The array of allowed values for validating the field.
+ * @param {string} fieldName - The name of the field for which validation is created.
+ * @returns {Object} The validation object with the isIn rule.
+ */
+const createValidationIsIn = (rule, fieldName) => ({
+  isIn: {
+    args: [rule], // Adding statuses for validation
+    msg: messages.error.requirements(fieldName),
+  },
+});
+
+module.exports = { validationRules, createValidation, createValidationIsIn };
