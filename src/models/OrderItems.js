69d4b25604edf14189139c49663e04bf15d11c15
Sam Malikin <workmalikin@gmail.com>
2024-10-17 22:38:52 +0300

    Updated OrderItem model with new fields and validations
    - Added new fields to the `OrderItem` model:
    
    - `quantity` — required field with validation for quantity of goods.
    - `unitPrice` — required field with validation for unit price of goods.
    - `totalPrice` — required field with validation for total price.
    
    - Added validations for each new field using custom `validationHandling` utility.
    - Changed table name to `orderitems`.

--- src/models/OrderItems.js
+++ src/models/OrderItems.js
@@ -1,8 +1,41 @@
 const sequelize = require("../config/dbConnect");
-const {DataTypes} = require("sequelize");
+const { DataTypes } = require("sequelize");
+const {
+  validationRules,
+  createValidation,
+} = require("../utils/validationHandling");
 
-const OrderItem = sequelize.define('orderItem', {
-    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true}
-})
+const OrderItem = sequelize.define(
+  "orderItem",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    quantity: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: createValidation(validationRules.orderItem.quantity, "quantity"),
+    },
+
+    unitPrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: createValidation(validationRules.orderItem.price, "unit price"),
+    },
+
+    totalPrice: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: createValidation(validationRules.orderItem.price, "total price"),
+    },
+  },
+  {
+    tableName: "orderitems",
+    timestamps: true,
+  }
+);
 
 module.exports = OrderItem;
