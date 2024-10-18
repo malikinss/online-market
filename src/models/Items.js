5b35166a74240ba35961ec3551decfc990f48a27
Sam Malikin <workmalikin@gmail.com>
2024-10-18 22:01:28 +0300

    Update Items.js

--- src/models/Items.js
+++ src/models/Items.js
@@ -1,48 +1,54 @@
 const sequelize = require("../config/dbConnect");
 const { DataTypes } = require("sequelize");
-const { validationRules, createValidation } = require("../utils/validationHandling");
+const {
+  validationRules,
+  createValidation,
+} = require("../utils/validationHandling");
 
-const Item = sequelize.define('item', {
+const Item = sequelize.define(
+  "item",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
 
     name: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull: false,
-        validate: createValidation(validationRules.item.name, "item name"),
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: createValidation(validationRules.item.name, "item name"),
     },
 
     description: {
-        type: DataTypes.TEXT,
-        allowNull: true
+      type: DataTypes.TEXT,
+      allowNull: true,
     },
 
     price: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        validate: createValidation(validationRules.item.price, "item price"),
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: createValidation(validationRules.item.price, "item price"),
     },
 
     stock: {
-        type: DataTypes.INTEGER,
-        defaultValue: 0,
-        validate: createValidation(validationRules.item.stock, "item stock"),
+      type: DataTypes.INTEGER,
+      defaultValue: 0,
+      validate: createValidation(validationRules.item.stock, "item stock"),
     },
 
     img: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: createValidation(validationRules.item.img, "item image"),
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: createValidation(validationRules.item.img, "item image"),
     },
-},
-{
-  tableName: "items",
-  timestamps: true,
-});
+  },
+  {
+    tableName: "items",
+    timestamps: true,
+  }
+);
 
 module.exports = Item;
