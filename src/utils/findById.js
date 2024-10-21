71d70c93f369a472ef77a65e7b5b01524a0dd7ec
Sam Malikin <workmalikin@gmail.com>
2024-10-21 21:43:06 +0300

    Create findById.js

---
+++ src/utils/findById.js
@@ -0,0 +1,26 @@
+const ApiError = require("../error/ApiError");
+
+/**
+ * Finds an element by its ID from the given model.
+ * @param {number|string} id - The ID of the element to find.
+ * @param {Object} Model - The database model to search in.
+ * @param {function} next - Express middleware function for handling errors.
+ * @returns {Object} The found element or an error if not found.
+ */
+async function findById(id, Model, next) {
+    try {
+        const element = await Model.findOne({ where: { id } });
+
+        if (!element) {
+            return next(ApiError.notFound("Element not found"));
+        }
+
+        return element;
+    } catch (error) {
+        return next(
+            ApiError.internal("An error occurred while fetching the element")
+        );
+    }
+}
+
+module.exports = { findById };
