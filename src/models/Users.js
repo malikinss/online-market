ac26e27aa77500d411de86c3f5a9e87b1d488ff3
Sam Malikin <workmalikin@gmail.com>
2024-10-23 09:37:22 +0300

    Update Users.js

--- src/models/Users.js
+++ src/models/Users.js
@@ -1,69 +1,87 @@
 const sequelize = require("../config/dbConnect");
 const { DataTypes } = require("sequelize");
 const {
-  validationRules,
-  createValidation,
-  createValidationIsIn,
+    validationRules,
+    createValidation,
+    createValidationIsIn,
 } = require("../utils/validationHandling");
 const { messages } = require("../views/messageHandling");
 
 const User = sequelize.define(
-  "user",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-
-    firstName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: createValidation(validationRules.user.firstName, "first name"),
-    },
-
-    lastName: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: createValidation(validationRules.user.lastName, "last name"),
-    },
-
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: {
-          msg: messages.error.requirements("email"),
+    "user",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
         },
-      },
-    },
 
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: createValidation(validationRules.user.password, "password"),
-    },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: createValidation(
+                validationRules.user.firstName,
+                "first name"
+            ),
+        },
 
-    phone: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: createValidation(validationRules.user.phone, "phone"),
-    },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: createValidation(
+                validationRules.user.lastName,
+                "last name"
+            ),
+        },
 
-    role: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "user",
-      validate: createValidationIsIn(validationRules.user.roles, "role"),
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: {
+                    msg: messages.error.requirements("email"),
+                },
+            },
+        },
+
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: createValidation(
+                validationRules.user.password,
+                "password"
+            ),
+        },
+
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: createValidation(validationRules.user.phone, "phone"),
+        },
+
+        role: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "user",
+            validate: createValidationIsIn(validationRules.user.roles, "role"),
+        },
+
+        addressId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "addresses",
+                key: "id",
+            },
+        },
     },
-  },
-  {
-    tableName: 'users',
-    timestamps: true,
-  }
+    {
+        tableName: "users",
+        timestamps: true,
+    }
 );
 
 module.exports = User;
