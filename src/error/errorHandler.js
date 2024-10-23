8e909185d31d7a3d622257328a34ef32c332c404
Sam Malikin <workmalikin@gmail.com>
2024-10-23 13:57:28 +0300

    Create errorHandler.js

---
+++ src/error/errorHandler.js
@@ -0,0 +1,14 @@
+const { handleError } = require("../controllers/userController");
+const ApiError = require("../error/ApiError");
+
+/**
+ * Handles errors and passes them to the next middleware.
+ * @param {Function} next - Express next middleware function.
+ * @param {string} message - Error message.
+ * @param {Error} error - Original error object.
+ */
+const handleError = (next, message, error) => {
+    return next(ApiError.internal(`Error ${message}: ${error.message}`));
+};
+
+module.exports = handleError;
