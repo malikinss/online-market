07689d426e503497ee8957efdbc7fd434f5d5a26
Sam Malikin <workmalikin@gmail.com>
2024-10-23 12:43:46 +0300

    Create userController.js
    Implement UserController for user management
    
    - Added `UserController` class to handle user registration, login, and management functionalities.
    - Implemented the following methods:
      - `registration`: Registers a new user, checks for required fields, validates uniqueness of email and phone, hashes the password, creates a user address, and generates a JWT token.
      - `login`: Authenticates a user by validating the email and password, then generates a JWT token.
      - `getCurrentUser`: Retrieves the current user's information based on the JWT token.
      - `changePassword`: Allows users to change their password after validating the old password and hashing the new one.
      - `updateUserInfo`: Updates the user's email and phone after checking for required fields and ensuring uniqueness.
      - `deleteUser`: Deletes a user and their associated address, ensuring proper handling of errors throughout the process.
    - Utilized helper functions for error handling, checking for falsy values, validating passwords, and checking user uniqueness.
    - Integrated the `ApiError` utility for consistent error handling across methods.

---
+++ src/controllers/userController.js
@@ -0,0 +1,226 @@
+const User = require("../models/Users");
+const UserAddress = require("../models/UserAddresses");
+const UserAddressController = require("../controllers/userAddressController");
+const bcrypt = require("bcrypt");
+const generateJWT = require("../utils/generateJWT");
+const findByField = require("../utils/findByField");
+const {
+    checkUserUniqueness,
+    checkForFalsyValues,
+    validatePassword,
+} = require("../utils/validationHandling");
+const ApiError = require("../error/ApiError");
+
+class UserController {
+    /**
+     * Handles errors and passes them to the next middleware.
+     * @param {Function} next - Express next middleware function.
+     * @param {string} message - Error message.
+     * @param {Error} error - Original error object.
+     */
+    handleError(next, message, error) {
+        return next(ApiError.internal(`Error ${message}: ${error.message}`));
+    }
+
+    /**
+     * Registers a new user.
+     * @param {Object} req - Express request object.
+     * @param {Object} res - Express response object.
+     * @param {Function} next - Express next middleware function.
+     * @returns {Object} - JSON containing JWT token.
+     */
+    async registration(req, res, next) {
+        try {
+            const {
+                firstName,
+                lastName,
+                email,
+                password,
+                phone,
+                role,
+                address,
+            } = req.body;
+
+            // Check for required fields
+            checkForFalsyValues(
+                [email, password, firstName, lastName, phone, address],
+                next
+            );
+
+            // Check email and phone uniqueness
+            await checkUserUniqueness(email, phone, null, next);
+
+            // Create user address
+            const newAddress = await UserAddressController.create(
+                req,
+                res,
+                next
+            );
+
+            // Hash the password
+            const hashedPassword = await bcrypt.hash(password, 5);
+
+            // Create a new user
+            const user = await User.create({
+                firstName,
+                lastName,
+                email,
+                password: hashedPassword,
+                phone,
+                role: role || "user",
+                addressId: newAddress.id,
+            });
+
+            // Generate JWT token and return it
+            const token = generateJWT(user.id, user.email, user.role);
+            return res.json({ token });
+        } catch (error) {
+            this.handleError(next, "creating user", error);
+        }
+    }
+
+    /**
+     * Logs in a user.
+     * @param {Object} req - Express request object.
+     * @param {Object} res - Express response object.
+     * @param {Function} next - Express next middleware function.
+     * @returns {Object} - JSON containing JWT token.
+     */
+    async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
+
+            // Check for required fields
+            checkForFalsyValues([email, password], next);
+
+            // Find user by email
+            const user = await findByField(email, User, next);
+
+            // Compare passwords
+            await validatePassword(password, user.password);
+
+            // Generate JWT token and return it
+            const token = generateJWT(user.id, user.email, user.role);
+            return res.json({ token });
+        } catch (error) {
+            this.handleError(next, "during authorization", error);
+        }
+    }
+
+    /**
+     * Retrieves the current user's information.
+     * @param {Object} req - Express request object.
+     * @param {Object} res - Express response object.
+     * @param {Function} next - Express next middleware function.
+     * @returns {Object} - JSON containing current user's information.
+     */
+    async getCurrentUser(req, res, next) {
+        try {
+            const userId = req.user.id; // Get user ID from token
+
+            // Find user by ID
+            const user = await findByField(userId, User, next);
+            return res.json(user);
+        } catch (error) {
+            this.handleError(next, "retrieving user information", error);
+        }
+    }
+
+    /**
+     * Changes the user's password.
+     * @param {Object} req - Express request object.
+     * @param {Object} res - Express response object.
+     * @param {Function} next - Express next middleware function.
+     * @returns {Object} - JSON containing success message.
+     */
+    async changePassword(req, res, next) {
+        try {
+            const { oldPassword, newPassword } = req.body;
+            const userId = req.user.id; // Get user ID from token
+
+            // Check for required fields
+            checkForFalsyValues([oldPassword, newPassword], next);
+
+            // Find user by ID
+            const user = await findByField(userId, User, next);
+
+            // Check old password
+            await validatePassword(oldPassword, user.password);
+
+            // Hash the new password and save it
+            user.password = await bcrypt.hash(newPassword, 5);
+            await user.save();
+
+            return res.json({ message: "Password successfully changed" });
+        } catch (error) {
+            this.handleError(next, "changing password", error);
+        }
+    }
+
+    /**
+     * Updates the user's information.
+     * @param {Object} req - Express request object.
+     * @param {Object} res - Express response object.
+     * @param {Function} next - Express next middleware function.
+     * @returns {Object} - JSON containing updated user information.
+     */
+    async updateUserInfo(req, res, next) {
+        try {
+            const { email, phone } = req.body;
+            const userId = req.user.id; // Get user ID from token
+
+            // Check for required fields
+            checkForFalsyValues([email, phone], next);
+
+            // Find user by ID
+            const user = await findByField(userId, User, next);
+
+            // Check email and phone uniqueness
+            await checkUserUniqueness(email, phone, userId, next);
+
+            // Update email and phone fields
+            user.email = email;
+            user.phone = phone;
+            await user.save();
+
+            return res.json(user);
+        } catch (error) {
+            this.handleError(next, "updating user information", error);
+        }
+    }
+
+    /**
+     * Deletes a user by ID.
+     * @param {Object} req - Express request object.
+     * @param {Object} res - Express response object.
+     * @param {Function} next - Express next middleware function.
+     * @returns {Object} - JSON containing a success message.
+     */
+    async deleteUser(req, res, next) {
+        try {
+            const userId = req.user.id; // Get user ID from token
+
+            // Find user by ID
+            const user = await findByField(userId, User, next);
+
+            // Find userAddress by ID
+            const address = await findByField(
+                user.addressId,
+                UserAddress,
+                next
+            );
+
+            // Delete the address
+            await UserAddressController.deleteById(address.id);
+
+            // Delete the user
+            await user.destroy();
+
+            return res.json({ message: "User successfully deleted" });
+        } catch (error) {
+            this.handleError(next, "deleting user", error);
+        }
+    }
+}
+
+module.exports = new UserController();
