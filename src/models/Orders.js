0c3503b2366670d34696a08d3c92ef6339566363
Sam Malikin <workmalikin@gmail.com>
2024-10-29 01:44:19 +0200

    Update Orders.js

--- src/models/Orders.js
+++ src/models/Orders.js
@@ -1,41 +1,56 @@
 const sequelize = require("../config/dbConnect");
 const { DataTypes } = require("sequelize");
 const {
-  validationRules,
-  createValidation,
-  createValidationIsIn,
+    validationRules,
+    createValidation,
+    createValidationIsIn,
 } = require("../utils/validationHandling");
 
 const Order = sequelize.define(
-  "order",
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
+    "order",
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
 
-    totalPrice: {
-      type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
-      validate: createValidation(validationRules.order.price, "total price"),
-    },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "users",
+                key: "id",
+            },
+            validate: {
+                isInt: true,
+            },
+        },
 
-    status: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: "Created",
-      validate: createValidationIsIn(
-        validationRules.order.status,
-        "order status"
-      ),
+        totalPrice: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+            validate: createValidation(
+                validationRules.order.price,
+                "total price"
+            ),
+        },
+
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "Created",
+            validate: createValidationIsIn(
+                validationRules.order.status,
+                "order status"
+            ),
+        },
     },
-  },
-  {
-    tableName: "orders",
-    timestamps: true,
-  }
+    {
+        tableName: "orders",
+        timestamps: true,
+    }
 );
 
 module.exports = Order;
