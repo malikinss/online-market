340c6d829a4bc3631dec7d89a9518b6b79a74fec
Sam Malikin <workmalikin@gmail.com>
2024-10-30 19:49:28 +0200

    Update userDelete.js

--- src/controllers/userController/methods/userDelete.js
+++ src/controllers/userController/methods/userDelete.js
@@ -1,6 +1,9 @@
 const User = require("../../../models/Users");
+const UserAddress = require("../../../models/UserAddresses");
 const UserAddressController = require("../../userAddressController");
-const findByField = require("../../../utils/findByField");
+
+const { findRecordByField } = require("../../controllerUtils/findHandlers");
+
 const ApiError = require("../../../error/ApiError");
 
 /**
@@ -11,14 +14,30 @@ const ApiError = require("../../../error/ApiError");
  * @returns {Object} - JSON containing a success message.
  */
 const userDelete = async (req, res, next) => {
+    const messages = {
+        success: "User successfully deleted",
+        error: {
+            find: {
+                user: "Failed to find user",
+                adress: "Failed to find user address",
+            },
+        },
+    };
+
     try {
         const userId = req.user.id; // Get user ID from token
 
         // Find user by ID
-        const user = await findByField(userId, User, next);
+        const user = await findRecordByField(userId, User);
+        if (!user) {
+            throw ApiError.badRequest(messages.error.find.user);
+        }
 
         // Find userAddress by ID
-        const address = await findByField(user.addressId, UserAddress, next);
+        const address = await findRecordByField(user.addressId, UserAddress);
+        if (!address) {
+            throw ApiError.badRequest(messages.error.find.adress);
+        }
 
         // Delete the address
         await UserAddressController.deleteById(address.id);
@@ -26,9 +45,9 @@ const userDelete = async (req, res, next) => {
         // Delete the user
         await user.destroy();
 
-        return res.json({ message: "User successfully deleted" });
+        return res.json({ message: messages.success });
     } catch (e) {
-        next(ApiError.badRequest(e.message));
+        return next(ApiError.badRequest(e.message));
     }
 };
 
