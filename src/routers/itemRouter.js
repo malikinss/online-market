8d1594d45978a87d6038186b8395ec0c1a947612
Sam Malikin <workmalikin@gmail.com>
2024-11-02 18:13:02 +0200

    Update itemRouter.js

--- src/routers/itemRouter.js
+++ src/routers/itemRouter.js
@@ -1,18 +1,18 @@
 const Router = require("express");
 const router = new Router();
-const itemController = require("../controllers/itemController");
+const ItemController = require("../controllers/itemController/itemController");
 const checkRole = require("../middleware/checkRoleMiddleware");
 
 // http://127.0.0.1:3000/api/item
 router
-  .route("/")
-  .post(checkRole("admin"), itemController.create) // POST Create item
-  .get(itemController.getAll); // GET all items
+    .route("/")
+    .post(checkRole("admin"), ItemController.createRecord) // POST Create item
+    .get(ItemController.getAllRecords); // GET all items
 
 router
-  .route("/:id")
-  .get(itemController.getOne) // GET item by id
-  .put(checkRole("admin"), itemController.updateById) // PUT item by id
-  .delete(checkRole("admin"), itemController.deleteById); // DELETE item by id
+    .route("/:id")
+    .get(ItemController.getRecord) // GET item by id
+    .put(checkRole("admin"), ItemController.updateRecord) // PUT item by id
+    .delete(checkRole("admin"), ItemController.deleteRecord); // DELETE item by id
 
 module.exports = router;
