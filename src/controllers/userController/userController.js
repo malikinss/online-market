7b1aeea82dda647535c43a9a5810e93f0b0bd17a
Sam Malikin <workmalikin@gmail.com>
2024-11-02 17:19:33 +0200

    Update userController.js

--- src/controllers/userController/userController.js
+++ src/controllers/userController/userController.js
@@ -1,36 +1,39 @@
-const userRegistration = require("./methods/userRegistration");
-const userLogIn = require("./methods/userLogIn");
+// Basic CRUDs
+const registerUser = require("./methods/registerUser");
 const getUser = require("./methods/getUser");
+const updateUser = require("./methods/updateUser");
+const deleteUser = require("./methods/deleteUser");
+
+// Additional funcs
+const logInUserHandler = require("./methods/logInUserHandler");
 const passwordChanger = require("./methods/passwordChanger");
-const userUpdate = require("./methods/userUpdate");
-const userDelete = require("./methods/userDelete");
 
 /**
  * Controller for managing users.
  */
 class UserController {
-    async registration(req, res, next) {
-        return userRegistration(req, res, next);
+    async createRecord(req, res, next) {
+        return registerUser(req, res, next);
     }
 
-    async login(req, res, next) {
-        return userLogIn(req, res, next);
-    }
-
-    async getCurrentUser(req, res, next) {
+    async getRecord(req, res, next) {
         return getUser(req, res, next);
+    }
+
+    async updateRecord(req, res, next) {
+        return updateUser(req, res, next);
+    }
+
+    async deleteRecord(req, res, next) {
+        return deleteUser(req, res, next);
+    }
+
+    async logIn(req, res, next) {
+        return logInUserHandler(req, res, next);
     }
 
     async changePassword(req, res, next) {
         return passwordChanger(req, res, next);
-    }
-
-    async updateUserInfo(req, res, next) {
-        return userUpdate(req, res, next);
-    }
-
-    async deleteUser(req, res, next) {
-        return userDelete(req, res, next);
     }
 }
 
