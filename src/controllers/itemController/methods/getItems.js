93eb0c5c6bf5b43c3f1f52240be32aa5c2300232
yuriy <iurie.rus@rambler.ru>
2024-11-03 16:05:20 +0200

    Update getItem

--- src/controllers/itemController/methods/getItems.js
+++ src/controllers/itemController/methods/getItems.js
@@ -1,14 +1,11 @@
 const Item = require("../../../models/Items");
 const ApiError = require("../../../error/ApiError");
 
-const {
-    containsFalsyValues,
-} = require("../../controllerUtils/dataValidations");
-const { findRecordsByField } = require("../../controllerUtils/findHandlers");
+const { findAllRecords } = require("../../controllerUtils/findHandlers");
 const { messages } = require("../../controllerUtils/messagesHandler");
 
 /**
- * Fetches items from the database based on the provided category ID.
+ * Fetches items from the database.
  * @param {Object} req - The request object.
  * @param {Object} res - The response object.
  * @param {function} next - The middleware function to call the next middleware in the stack.
@@ -17,34 +14,29 @@ const { messages } = require("../../controllerUtils/messagesHandler");
  * @returns {Promise<void>} - Returns a JSON response with the found items or calls the next middleware with an error.
  */
 const getItems = async (req, res, next) => {
-    try {
-        const { categoryID } = req.query;
+  try {
+    // Attempt to find the items in the database
+    const items = await findAllRecords(Item);
 
-        // Validate input to ensure no falsy values
-        containsFalsyValues([categoryID]);
-
-        // Attempt to find the items in the database
-        const items = await findRecordsByField("categoryId", categoryID, Item);
-
-        // If the items are not found, throw a not found error
-        if (!items) {
-            throw new ApiError.notFound(
-                messages.errors.actionFailed("find", "Items")
-            );
-        }
-
-        // Log success message to the console
-        console.log(messages.success("Item", "found"));
-
-        // Return the found items as a JSON response
-        return res.json(items);
-    } catch (e) {
-        next(
-            ApiError.badRequest(
-                messages.errors.general("fetching", "Items", e.message)
-            )
-        );
+    // If the items are not found, throw a not found error
+    if (!items) {
+      throw new ApiError.notFound(
+        messages.errors.actionFailed("find", "Items")
+      );
     }
+
+    // Log success message to the console
+    console.log(messages.success("Item", "found"));
+
+    // Return the found items as a JSON response
+    return res.json(items);
+  } catch (e) {
+    next(
+      ApiError.badRequest(
+        messages.errors.general("fetching", "Items", e.message)
+      )
+    );
+  }
 };
 
 module.exports = getItems;
