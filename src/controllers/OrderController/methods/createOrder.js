6a4251b7114e3094933f50edd20f87692a215bb3
yuriy <iurie.rus@rambler.ru>
2024-11-04 01:20:08 +0200

    Update OrderItem

--- src/controllers/OrderController/methods/createOrder.js
+++ src/controllers/OrderController/methods/createOrder.js
@@ -4,7 +4,7 @@ const OrderItemController = require("../../OrderItemController/OrderItemControll
 const PaymentController = require("../../PaymentController/PaymentController");
 
 const {
-    containsFalsyValues,
+  containsFalsyValues,
 } = require("../../controllerUtils/dataValidations");
 const { messages } = require("../../controllerUtils/messagesHandler");
 
@@ -15,68 +15,67 @@ const { messages } = require("../../controllerUtils/messagesHandler");
  * @param {Function} next - The next middleware function.
  */
 const createOrder = async (req, res, next) => {
-    try {
-        const { userId, orderItems } = req.body;
+  try {
+    const { userId, orderItems } = req.body;
 
-        // Check for falsy values in the order fields
-        containsFalsyValues([userId, orderItems]);
+    // Check for falsy values in the order fields
+    containsFalsyValues([userId, orderItems]);
 
-        // Create a new Order in the database
-        const newOrder = await Order.create({
-            userId,
-            totalPrice: 0,
-            status: "Unpaid",
-        });
-        if (!newOrder) {
-            throw new ApiError.internal(
-                messages.errors.actionFailed("create", "Order")
-            );
-        }
-
-        res.locals.orderId = newOrder.id;
-
-        // Create a new Payment in the database
-        await PaymentController.createRecord(req, res, next);
-        const newPayment = res.locals.payment;
-        if (!newPayment) {
-            throw new ApiError.internal(
-                messages.errors.actionFailed("create", "OrderId")
-            );
-        }
-
-        // Create OrderItems
-        const createdOrderItems = [];
-        for (let orderItemData of orderItems) {
-            res.locals.orderItemReq = orderItemData;
-
-            // Create a new OrderItem in the database
-            await OrderItemController.createRecord(req, res, next);
-            let newOrderItem = res.locals.orderItem;
-
-            if (!newOrderItem) {
-                throw new ApiError.internal(
-                    messages.errors.actionFailed("create", "OrderItem")
-                );
-            }
-
-            createdOrderItems.push(newOrderItem);
-        }
-
-        // Log success message
-        console.log(messages.success("Order", "created"));
-
-        return res.json({
-            order: newOrder,
-            items: createdOrderItems,
-            payment: newPayment,
-        });
-    } catch (e) {
-        next(
-            ApiError.badRequest(
-                messages.errors.general("creating", "Order", e.message)
-            )
-        );
+    // Create a new Order in the database
+    const newOrder = await Order.create({
+      userId,
+      totalPrice: 0,
+      status: "Unpaid",
+    });
+    if (!newOrder) {
+      throw ApiError.internal(messages.errors.actionFailed("create", "Order"));
     }
+
+    res.locals.orderId = newOrder.id;
+
+    // Create OrderItems
+    const createdOrderItems = [];
+    for (let orderItemData of orderItems) {
+      res.locals.orderItemReq = orderItemData;
+
+      // Create a new OrderItem in the database
+      await OrderItemController.createRecord(req, res, next);
+      let newOrderItem = res.locals.orderItem;
+
+      if (!newOrderItem) {
+        throw ApiError.internal(
+          messages.errors.actionFailed("create", "OrderItem")
+        );
+      }
+
+      createdOrderItems.push(newOrderItem);
+    }
+
+    // Create a new Payment in the database
+    await PaymentController.createRecord(req, res, next);
+    const newPaymentId = res.locals.paymentId;
+
+    if (!newPaymentId) {
+      throw ApiError.internal(
+        messages.errors.actionFailed("create", "Payment")
+      );
+    }
+
+    // Log success message
+    console.log(messages.success("Order", "created"));
+
+    return res.json({
+      order: newOrder,
+      items: createdOrderItems,
+      paymentId: newPaymentId,
+    });
+  } catch (e) {
+    next(
+      ApiError.badRequest(
+        messages.errors.general("creating", "Order", e.message)
+      )
+    );
+  }
 };
 
 module.exports = createOrder;
--- src/controllers/PaymentController/methods/createPayment.js
+++ src/controllers/PaymentController/methods/createPayment.js
@@ -9,33 +9,30 @@ const { messages } = require("../../controllerUtils/messagesHandler");
  * @param {Function} next - The next middleware function.
  */
 const createPayment = async (req, res, next) => {
-    try {
-        const orderID = req.body.id || res.locals.orderId;
-        if (!orderID) {
-            throw new ApiError.badRequest(
-                messages.errors.nullData("Order", "id")
-            );
-        }
-
-        // Create a new payment in the database
-        const payment = await Payment.create({ orderID, status: false });
-        if (!payment) {
-            throw new ApiError.internal(
-                messages.errors.actionFailed("create", "Payment")
-            );
-        }
-
-        res.locals.payment = payment;
-
-        // Log success message
-        console.log(messages.success("Payment", "created"));
-    } catch (e) {
-        next(
-            ApiError.badRequest(
-                messages.errors.general("creating", "payment", e.message)
-            )
-        );
+  try {
+    const orderID = req.body.id || res.locals.orderId;
+    if (!orderID) {
+      throw ApiError.badRequest(messages.errors.nullData("Order", "id"));
     }
+
+    // Create a new payment in the database
+    const payment = await Payment.create({ orderID, status: false });
+    if (!payment) {
+      throw ApiError.internal(
+        messages.errors.actionFailed("create", "Payment")
+      );
+    }
+    const paymentId = payment.dataValues.id;
+    res.locals.paymentId = paymentId;
+    // Log success message
+    console.log(messages.success("Payment", "created"));
+  } catch (e) {
+    next(
+      ApiError.badRequest(
+        messages.errors.general("creating", "payment", e.message)
+      )
+    );
+  }
 };
 
 module.exports = createPayment;
