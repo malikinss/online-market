3de4c96b8b2c1a47c56d1ea991ee6986f48820dc
Sam Malikin <workmalikin@gmail.com>
2024-11-03 21:15:47 +0200

    Update getOrder.js

--- src/controllers/OrderController/methods/getOrder.js
+++ src/controllers/OrderController/methods/getOrder.js
@@ -1,17 +1,43 @@
-/**
-     * Get an order by ID.
-     * @param {Object} req - The request object.
-     * @param {Object} res - The response object.
-     * @param {Function} next - The next middleware function.
-     */
-async getOrderById(req, res, next) {
-    try {
-        const id = req.params.id;
+const Order = require("../../../models/Orders");
+const ApiError = require("../../../error/ApiError");
 
-        const order = await findByField(id, Order, next);
+const { findRecordByField } = require("../../controllerUtils/findHandlers");
+const { messages } = require("../../controllerUtils/messagesHandler");
+
+/**
+ * Get an order by ID.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
+const getOrder = async (req, res, next) => {
+    try {
+        const orderID = req.params.id;
+        if (!orderID) {
+            throw new ApiError.badRequest(
+                messages.errors.nullData("Order", "id")
+            );
+        }
+
+        // Log success message
+        console.log(messages.success("Order", "found"));
+
+        const order = await findRecordByField("id", orderID, Order);
+        if (!order) {
+            throw new ApiError.notFound(
+                messages.errors.actionFailed("find", "Order")
+            );
+        }
+
+        // Log success message to the console
+        console.log(messages.success("Order", "found"));
 
         return res.json(order);
     } catch (e) {
-        next(ApiError.badRequest(e.message));
+        return next(
+            ApiError.internal(messages.general("finding", "Order", e.message))
+        );
     }
-}
+};
+
+module.exports = getOrder;
