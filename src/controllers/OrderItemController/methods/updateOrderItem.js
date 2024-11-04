2599031555d5bfb4672026d1adad435cf581fd93
Sam Malikin <workmalikin@gmail.com>
2024-11-04 20:33:51 +0200

    Update updateOrderItem.js

--- src/controllers/OrderItemController/methods/updateOrderItem.js
+++ src/controllers/OrderItemController/methods/updateOrderItem.js
@@ -1,33 +1,74 @@
+const OrderItem = require("../../../models/OrderItems");
+const Order = require("../../../models/Orders");
+const ApiError = require("../../../error/ApiError");
+
+const {
+    containsFalsyValues,
+} = require("../../controllerUtils/dataValidations");
+const { findRecordByField } = require("../../controllerUtils/findHandlers");
+const { messages } = require("../../controllerUtils/messagesHandler");
+
 /**
- * Update an orderItem by ID.
+ * Update an order item by ID.
  * @param {Object} req - The request object.
+ * @param {string} req.params.id - The ID of the order item to update.
+ * @param {Object} req.body - The body of the request.
+ * @param {number} req.body.quantity - The new quantity for the order item.
  * @param {Object} res - The response object.
  * @param {Function} next - The next middleware function.
  */
-const updatOrderItem = async (req, res, next) => {
+const updateOrderItem = async (req, res, next) => {
     try {
-        const id = req.params.id;
-        const { quantity } = req.body;
+        const orderItemId = req.params.id;
+        const newQuantity = req.body.quantity;
 
-        // Check for falsy values in the payment fields
-        checkForFalsyValues([quantity], next);
+        // Validate input to ensure no falsy values
+        containsFalsyValues([orderItemId, newQuantity]);
 
-        const orderItem = await findByField(id, OrderItem, next);
+        // Find the OrderItem by ID
+        const orderItem = await findRecordByField("id", orderItemId, OrderItem);
+        if (!orderItem) {
+            throw ApiError.notFound(
+                messages.errors.actionFailed("find", "OrderItem")
+            );
+        }
+
+        // Get current totalPrice (quantity * unitPrice)
+        const oldOrderItemTotalPrice = orderItem.totalPrice;
 
         // Update quantity and trigger hooks to adjust the totalPrice
-        orderItem.quantity = quantity;
+        orderItem.quantity = newQuantity;
         await orderItem.save();
 
-        // Update totalPrice in Order
-        const order = await Order.findByPk(orderItem.orderId);
-        order.totalPrice =
-            order.totalPrice - oldTotalPrice + orderItem.totalPrice;
+        // Get updated totalPrice (newQuantity * unitPrice)
+        const newOrderItemTotalPrice = orderItem.totalPrice;
+
+        // Find the associated Order by ID
+        const order = await findRecordByField("id", orderItem.orderId, Order);
+        if (!order) {
+            throw ApiError.notFound(
+                messages.errors.actionFailed("find", "Order")
+            );
+        }
+
+        // Calculate and update the new total price of the Order
+        const newOrderTotalPrice =
+            order.totalPrice - oldOrderItemTotalPrice + newOrderItemTotalPrice;
+        order.totalPrice = newOrderTotalPrice;
         await order.save();
 
+        // Log success message to the console
+        console.log(messages.success("OrderItem", "update"));
+
+        // Return the Updated OrderItem as a JSON response
         return res.json(orderItem);
     } catch (e) {
-        next(ApiError.badRequest(e.message));
+        next(
+            ApiError.badRequest(
+                messages.errors.general("updating", "OrderItem", e.message)
+            )
+        );
     }
 };
 
-module.exports = updatOrderItem;
+module.exports = updateOrderItem;
