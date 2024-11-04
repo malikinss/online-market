75ae25d5de388ea321887fd2ee118674f08305d7
Sam Malikin <workmalikin@gmail.com>
2024-11-04 20:14:41 +0200

    Update getCategories.js

--- src/controllers/CategoryController/methods/getCategories.js
+++ src/controllers/CategoryController/methods/getCategories.js
@@ -5,17 +5,21 @@ const { findAllRecords } = require("../../controllerUtils/findHandlers");
 const { messages } = require("../../controllerUtils/messagesHandler");
 
 /**
- * Retrieves all categories from the database and returns them as JSON.
- * @param {Object} req - Express request object.
- * @param {Object} res - Express response object to send the result.
- * @param {Function} next - Express middleware function for error handling.
- * @returns {Promise<Object>} JSON array of categories.
+ * Retrieves all categories from the database and returns them as a JSON array.
+ * This function serves as middleware in an Express.js application, handling incoming requests to fetch all category records.
+ * It performs validation checks and returns appropriate error messages if the categories are not found or if an internal error occurs during fetching.
+ * @param {Object} req - The Express request object.
+ * @param {Object} res - The Express response object.
+ * @param {Function} next - The next middleware function in the Express middleware chain.
+ * @throws {ApiError} If no categories are found in the database, a not found error is thrown.
+ * @throws {ApiError} If an error occurs while fetching categories, an internal error is thrown.
  */
 const getCategories = async (req, res, next) => {
     try {
+        // Find all categories from the database
         const categories = await findAllRecords(Category);
         if (!categories) {
-            throw new ApiError.notFound(
+            throw ApiError.notFound(
                 messages.errors.actionFailed("find", "Categories")
             );
         }
@@ -23,6 +27,7 @@ const getCategories = async (req, res, next) => {
         // Log success message
         console.log(messages.success("Categories", "found"));
 
+        // Return the categories as a JSON response
         return res.json(categories);
     } catch (e) {
         return next(
