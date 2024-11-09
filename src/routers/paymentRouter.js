85a62bffa417326398dd2303864b1eb630a90880
Sam Malikin <workmalikin@gmail.com>
2024-11-09 06:22:56 +0200

    Update paymentRouter.js

--- src/routers/paymentRouter.js
+++ src/routers/paymentRouter.js
@@ -1,13 +1,16 @@
 const express = require("express");
 const router = express.Router();
+
 const PaymentController = require("../controllers/PaymentController/PaymentController");
 
+// POST: Create a new payment record
 router.post("/", PaymentController.createRecord);
 
+// GET, PUT, DELETE: Operations with a specific payment record by ID
 router
     .route("/:id")
-    .get(PaymentController.getRecord)
-    .put(PaymentController.updateRecord)
-    .delete(PaymentController.deleteRecord);
+    .get(PaymentController.getRecord) // GET: Get payment information by ID
+    .put(PaymentController.updateRecord) // PUT: Update payment record by ID
+    .delete(PaymentController.deleteRecord); // DELETE: Удаление платежной записи по ID
 
 module.exports = router;
