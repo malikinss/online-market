98c54296013471342df0edd09543cd279c9e5095
Sam Malikin <workmalikin@gmail.com>
2024-11-09 06:12:59 +0200

    Update orderItemRouter.js

--- src/routers/orderItemRouter.js
+++ src/routers/orderItemRouter.js
@@ -3,15 +3,17 @@ const router = express.Router();
 
 const OrderItemController = require("../controllers/OrderItemController/OrderItemController");
 
+// Routes for creating and retrieving order items
 router
-  .route("/")
-  .post(OrderItemController.createRecord)
-  .get(OrderItemController.getRecords);
+    .route("/")
+    .post(OrderItemController.createRecord) // POST: Create an order item record
+    .get(OrderItemController.getRecords); // GET: Get all order item records
 
+// Routes for specific order item operations by ID
 router
-  .route("/:id")
-  .get(OrderItemController.getRecord)
-  .put(OrderItemController.updateRecord)
-  .delete(OrderItemController.deleteRecord);
+    .route("/:id")
+    .get(OrderItemController.getRecord) // GET: Get order item by ID
+    .put(OrderItemController.updateRecord) // PUT: Update order item by ID
+    .delete(OrderItemController.deleteRecord); // DELETE: Delete an order item by ID
 
 module.exports = router;
