409419c4d49376b4cf619e0454a3cb8dbf11dcbb
Sam Malikin <workmalikin@gmail.com>
2024-11-09 06:20:12 +0200

    Update orderRouter.js

--- src/routers/orderRouter.js
+++ src/routers/orderRouter.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const router = express.Router();
+
+const checkRole = require("../middleware/checkRoleMiddleware");
 const OrderController = require("../controllers/OrderController/OrderController");
 
+// Routes for creating and receiving all orders
 router
-  .route("/")
-  .post(OrderController.createRecord)
-  .get(OrderController.getRecords);
+    .route("/")
+    .post(OrderController.createRecord) // POST: Create a new order
+    .get(checkRole("admin"), OrderController.getRecords); // GET: Get all orders (admin only)
 
+// Routes for operations with a specific order by ID
 router
-  .route("/:id")
-  .get(OrderController.getRecord)
-  .put(OrderController.updateRecord)
-  .delete(OrderController.deleteRecord);
+    .route("/:id")
+    .get(OrderController.getRecord) // GET: Get order by ID
+    .put(OrderController.updateRecord) // PUT: Update order by ID
+    .delete(OrderController.deleteRecord); // DELETE: Delete order by ID
 
-router.route("/user/:id").get(OrderController.getRecordsPerUser);
+// Route for receiving orders of a specific user
+router.route("/user/:id").get(OrderController.getRecordsPerUser); // GET: Get orders by user ID
 
 module.exports = router;
