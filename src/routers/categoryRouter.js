af5e8e1c7d39247525bc4e239ef006651403d780
Sam Malikin <workmalikin@gmail.com>
2024-11-09 06:04:41 +0200

    Update categoryRouter.js

--- src/routers/categoryRouter.js
+++ src/routers/categoryRouter.js
@@ -1,16 +1,19 @@
-const Router = require("express");
-const router = new Router();
+const express = require("express");
+const router = express.Router();
+
 const checkRole = require("../middleware/checkRoleMiddleware");
 const CategoryController = require("../controllers/CategoryController/CategoryController");
 
+// Creating and getting categories
 router
     .route("/")
-    .post(checkRole("admin"), CategoryController.createRecord)
-    .get(CategoryController.getRecords);
+    .post(checkRole("admin"), CategoryController.createRecord) // Create category (admin only)
+    .get(CategoryController.getRecords); // Get all categories
 
+// Updating and deleting a category by ID
 router
     .route("/:id")
-    .put(checkRole("admin"), CategoryController.updateRecord)
-    .delete(checkRole("admin"), CategoryController.deleteRecord);
+    .put(checkRole("admin"), CategoryController.updateRecord) // Category update (admin only)
+    .delete(checkRole("admin"), CategoryController.deleteRecord); // Delete category (admin only)
 
 module.exports = router;
