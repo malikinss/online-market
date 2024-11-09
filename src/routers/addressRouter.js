5c9c4be020cfaa98d428f9491a68f9b3b442a764
Sam Malikin <workmalikin@gmail.com>
2024-11-09 06:00:34 +0200

    Update addressRouter.js

--- src/routers/addressRouter.js
+++ src/routers/addressRouter.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const UserAddressController = require("../controllers/UserAddressController/UserAddressController");
 
-// Маршрут для создания нового адреса
+// Create a new address
 router.post("/create", UserAddressController.createRecord);
 
+// Getting, updating and deleting an address by ID
 router
-  .route("/:id")
-  .get(UserAddressController.getRecord)
-  .put(UserAddressController.updateRecord)
-  .delete(UserAddressController.deleteRecord);
+    .route("/:id")
+    .get(UserAddressController.getRecord) // Getting address by ID
+    .put(UserAddressController.updateRecord) // Updating address by ID
+    .delete(UserAddressController.deleteRecord); // Delete address by ID
 
 module.exports = router;
