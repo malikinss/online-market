b38d4db098faf6a5f1c9d6649161bca6c30cd59a
Sam Malikin <workmalikin@gmail.com>
2024-11-09 06:04:38 +0200

    Update addressRouter.js

--- src/routers/addressRouter.js
+++ src/routers/addressRouter.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+
 const UserAddressController = require("../controllers/UserAddressController/UserAddressController");
 
 // Create a new address
